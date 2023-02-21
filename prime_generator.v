9044216229b6c43e835e2ea80f0944352e0e9ef9
ENGR 850 04 <engr850-04@hafez.sfsu.edu>
2023-02-20 20:26:21 -0800

    Quick Add

---
+++ prime_generator.v
@@ -0,0 +1,75 @@
+module prime_generator(
+  input clk,
+  output reg [4095:0] prime
+);
+
+  // Function to perform modular exponentiation
+  function automatic integer modular_exponentiation;
+    input [4095:0] base, exponent, modulus;
+    reg [4095:0] result = 1;
+    for (int i = 4095; i >= 0; i = i - 1) begin
+      result = (result * result) % modulus;
+      if (exponent[i] == 1) begin
+        result = (result * base) % modulus;
+      end
+    end
+    return result;
+  endfunction
+
+  // Function to perform the Miller-Rabin primality test
+  function automatic integer miller_rabin_test;
+    input [4095:0] n, a;
+    reg [4095:0] m = n - 1;
+    reg [4095:0] y = modular_exponentiation(a, m, n);
+    while (m != 1 && y != 1 && y != n - 1) begin
+      y = (y * y) % n;
+      m = m / 2;
+    end
+    if (y != n - 1 && m % 2 == 0) begin
+      return 0; // n is composite
+    end
+    return 1; // n is probably prime
+  endfunction
+
+  // Function to perform the AKS primality test
+  function automatic integer aks_test;
+    input [4095:0] n;
+    reg [4095:0] c;
+    for (int i = 1; i <= 4095/2; i = i + 1) begin
+      c[i] = 1;
+      for (int j = 1; j <= i; j = j + 1) begin
+        c[i+1-j] = ((c[i+1-j] + c[i-j]) * (i-j+1)) / j;
+      end
+    end
+    c[0] = 1;
+    for (int r = 1; r <= 4095/2; r = r + 1) begin
+      if (n % r == 0) begin
+        continue;
+      end
+      reg [4095:0] x = modular_exponentiation(2, r, n);
+      if (x == 1 || x == n - 1) begin
+        continue;
+      end
+      reg [4095:0] xp = x;
+      for (int i = 1; i < c[r]; i = i + 1) begin
+        x = (x * xp) % n;
+      end
+      if (x == 1) begin
+        return 0; // n is composite
+      end
+      if (x == n - 1) begin
+        continue;
+      end
+      for (int i = 1; i < r; i = i + 1) begin
+        x = (x * x) % n;
+        if (x == n - 1) begin
+          continue 'r;
+        end
+        if (x == 1) begin
+          return 0; // n is composite
+        end
+      end
+      return 0; // n is composite
+    end
+    return 1; // n is prime
+  endfunction
---
+++ prime_generator_tb.sv
@@ -0,0 +1,53 @@
+module prime_generator_tb;
+
+  // Test input signals
+  reg clk;
+  reg rst;
+  
+  // Test output signals
+  wire [4095:0] prime;
+
+  // Instantiate the prime generator module
+  prime_generator dut(
+    .clk(clk),
+    .rst(rst),
+    .prime(prime)
+  );
+
+  initial begin
+    // Initialize inputs
+    clk = 0;
+    rst = 1;
+
+    // Wait for a few cycles
+    #10;
+
+    // De-assert reset
+    rst = 0;
+
+    // Wait for a few cycles
+    #10;
+
+    // Set a random seed value for the generator
+    $random(seed = $time);
+
+    // Generate a prime number and validate it
+    repeat (10) begin
+      // Wait for a few cycles
+      #10;
+
+      // Toggle the clock signal
+      clk = ~clk;
+
+      // Wait for a few cycles
+      #10;
+
+      // Check if the generated prime number is valid
+      if (prime !== 0) begin
+        $display("Valid prime number found: %d", prime);
+        // Stop the simulation
+        $finish;
+      end
+    end
+  end
+endmodule
